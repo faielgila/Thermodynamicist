480ef97b7fdaf57daf5a387ea6a4a23a281b12db
minec <faielgila@gmail.com>
2022-07-18 00:16:44 -0700

    Changed hardcoded input to dynamic input

--- ThermodynamicistUWP/MainPage.xaml.cs
+++ ThermodynamicistUWP/MainPage.xaml.cs
@@ -20,34 +20,46 @@ using Core.VariableTypes;
 
 namespace ThermodynamicistUWP
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		private void ClickRecalculate()
         {
-            this.InitializeComponent();
+			
+		}
 
-            PengRobinsonEOS PREoS = new PengRobinsonEOS(Chemical.Water);
-            var T = new Temperature(500);
-            var P = new Pressure(101.325e3);
-            var phaseVMols = PREoS.PhaseFinder(T, P, true);
-            var phaseEquilibriums = PREoS.IsStateInPhaseEquilbirum(T, P, phaseVMols.L, phaseVMols.V);
+		private void UpdateData(CubicEquationOfState EoS, Temperature T, Pressure P)
+		{
+			var phaseVMols = EoS.PhaseFinder(T, P, true);
+			var phaseEquilibriums = EoS.IsStateInPhaseEquilbirum(T, P, phaseVMols.L, phaseVMols.V);
 
-            string phasesString = "";
-            if ( phaseEquilibriums.L &&  phaseEquilibriums.V) phasesString = "liquid, vapor";
-            if (!phaseEquilibriums.L &&  phaseEquilibriums.V) phasesString = "vapor";
-            if ( phaseEquilibriums.L && !phaseEquilibriums.V) phasesString = "liquid";
+			string phasesString = "";
+			if (phaseEquilibriums.L && phaseEquilibriums.V) phasesString = "liquid, vapor";
+			if (!phaseEquilibriums.L && phaseEquilibriums.V) phasesString = "vapor";
+			if (phaseEquilibriums.L && !phaseEquilibriums.V) phasesString = "liquid";
 
-            var stateData =
-                "Reference state: (" + 298.15.ToEngrNotation() + " K, " + 100e3.ToEngrNotation() + " Pa) \n" +
-                "Interested state: (" + (double)T + " K, " + (double)P + " Pa) \n" +
-                "Phases at equilibrium: " + phasesString;
+			var stateData =
+				"Reference state: (" + 298.15.ToEngrNotation() + " K, " + 100e3.ToEngrNotation() + " Pa) \n" +
+				"Phases at equilibrium: " + phasesString;
 
-            DataLabel.Text = stateData;
-            GroupBoxVapor.Text = "Vapor phase data: \n" + Display.GetAllStateVariablesFormatted(PREoS, T, P, phaseVMols.V, 5);
-            GroupBoxLiquid.Text = "Liquid phase data: \n" + Display.GetAllStateVariablesFormatted(PREoS, T, P, phaseVMols.L, 5);
-        }
+			DataLabel.Text = stateData;
+			GroupBoxVapor.Text = "Vapor phase data: \n" + Display.GetAllStateVariablesFormatted(EoS, T, P, phaseVMols.V, 5);
+			GroupBoxLiquid.Text = "Liquid phase data: \n" + Display.GetAllStateVariablesFormatted(EoS, T, P, phaseVMols.L, 5);
+		}
+
+        private void ButtonRecalc_Click(object sender, RoutedEventArgs e)
+        {
+			var T = new Temperature(NumBoxT.Value);
+			var P = new Pressure(NumBoxP.Value);
+			PengRobinsonEOS PREoS = new PengRobinsonEOS(Chemical.Water);
+			UpdateData(PREoS, T, P);
+		}
     }
 }
